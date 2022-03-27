5c5883ff1268f101101461c2d756c8f0c9c81964
sammychvz <sammy_chvz17@outlook.com>
2022-03-27 15:30:15 -0500

    added the cache to the service worker and added the index.db for mongoose

---
+++ Develop/public/js/idb.js
@@ -0,0 +1,4 @@
+let db;
+const request = indexedDB.open('money_movers', 1);
+
+
---
+++ Develop/public/service-worker.js
@@ -0,0 +1,5 @@
+const APP_PREFIX = 'MoneyMovers-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
+
+  
